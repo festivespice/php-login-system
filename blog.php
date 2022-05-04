0406e0ef4f692350bb4e318868bebb72b33ab4a7
Rafael <nunezduringrafael@gmail.com>
2022-05-04 15:35:18 -0400

    Initial project state plain HTML

---
+++ blog.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP Login System</title>
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;800&display=swap" rel="stylesheet">    </head>
+    <body>
+        <div class="wrap">
+            <div class="main">
+                <div class="topnav">
+                    <h2 href="index.php"> Something </h2>
+
+                    <a href="signin.php">Log In</a>
+                    <a href="signup.php">Sign Up</a>
+                    <a class="active" href="blog.php">Find Blogs</a>
+                    <a href="discover.php">About Us</a>
+                    <a href="index.php">Home</a>
+                </div>
+
+                <div class="currentPageContentSized">
+                    <h1> Hello, this is content! </h1>
+                    <p> 
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor vitae purus faucibus ornare suspendisse sed nisi. Velit aliquet sagittis id consectetur purus. Adipiscing diam donec adipiscing tristique risus nec. Pharetra et ultrices neque ornare aenean euismod elementum nisi quis. Bibendum enim facilisis gravida neque convallis a cras semper auctor. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Hac habitasse platea dictumst vestibulum rhoncus. Varius vel pharetra vel turpis nunc eget lorem. Volutpat commodo sed egestas egestas fringilla. Consectetur lorem donec massa sapien faucibus et molestie ac feugiat. Commodo nulla facilisi nullam vehicula. Libero volutpat sed cras ornare arcu dui vivamus arcu felis. Sit amet aliquam id diam. Tristique risus nec feugiat in.
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+        <footer>
+            <p>Rafael Nunez @2022</p>
+        </footer>
+    </body>
+</html>
---
+++ signin.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP Login System</title>
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;800&display=swap" rel="stylesheet">    </head>
+    <body>
+        <div class="wrap">
+            <div class="main">
+                <div class="topnav">
+                    <h2 href="index.php"> Something </h2>
+
+                    <a class="active" href="signin.php">Log In</a>
+                    <a href="signup.php">Sign Up</a>
+                    <a href="blog.php">Find Blogs</a>
+                    <a href="discover.php">About Us</a>
+                    <a href="index.php">Home</a>
+                </div>
+
+                <div class="currentPageContentSized">
+                    <h1>Log in</h1>
+                </div>
+            </div>
+        </div>
+        
+        <footer>
+            <p>Rafael Nunez @2022</p>
+        </footer>
+    </body>
+</html>
